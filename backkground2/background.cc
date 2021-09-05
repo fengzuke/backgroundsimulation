79839544bfaab96e5eecb0595b5d9341f38a46b2
fengzuke <87075074+fengzuke@users.noreply.github.com>
2021-09-05 16:08:55 +0800

    Add files via upload

--- backkground2/background.cc
+++ backkground2/background.cc
@@ -1,33 +1,3 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-// $Id: background.cc 86065 2014-11-07 08:51:15Z gcosmo $
-//
-/// \file background.cc
-/// \brief Main program of the background example
-
 #include "backgroundDetectorConstruction.hh"
 #include "backgroundActionInitialization.hh"
 
@@ -57,7 +27,12 @@ int main(int argc,char** argv)
   }
 
   // Choose the Random engine
-  G4Random::setTheEngine(new CLHEP::RanecuEngine);
+  //先选随机数引擎，如果不选，默认是HepJamesRandom引擎。
+  CLHEP::HepRandom::setTheEngine(new CLHEP::RanecuEngine());
+
+  //设定随机数种子
+  G4long seed = time(NULL);
+  CLHEP::HepRandom::setTheSeed(seed);
   
   // Construct the default run manager
   //
